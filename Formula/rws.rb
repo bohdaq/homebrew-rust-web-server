f4d1f40156e55e95062f92cc32d04e820ca809a5
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-10-16 14:38:17 +0300

    update

--- Formula/rws.rb
+++ Formula/rws.rb
@@ -1,7 +1,4 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class RustWebServer < Formula
+class Rws < Formula
   desc "Hi, rust-web-server (rws) is a simple web-server written in Rust. The rws server can serve static content inside the directory it is started."
   homepage ""
   url "https://github.com/bohdaq/rust-web-server/archive/refs/tags/0.0.28.tar.gz"
@@ -11,20 +8,10 @@ class RustWebServer < Formula
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test rust-web-server`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
