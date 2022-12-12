134222b969a8cba293c813202ea8e93317a2fe3b
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-12 06:51:06 +0200

    8.0.0

--- Formula/rws.rb
+++ Formula/rws.rb
@@ -1,8 +1,8 @@
 class Rws < Formula
   desc "Hi, rust-web-server (rws) is a simple web-server written in Rust. The rws server can serve static content inside the directory it is started."
   homepage ""
-  url "https://github.com/bohdaq/rust-web-server/archive/refs/tags/0.0.28.tar.gz"
-  sha256 "3d9814c2eb1bdddee8a27519ce3b04c24cb049ba756e8bfc293811b1809fca74"
+  url "https://github.com/bohdaq/rust-web-server/archive/refs/tags/8.0.0.tar.gz"
+  sha256 "e1aad1e46eb0deaaf8842e74e1e058b64525361d4fcd67371775a85b7f724a96"
   license "MIT"
 
   depends_on "rust" => :build
